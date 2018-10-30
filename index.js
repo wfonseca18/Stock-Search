2f48ac3417a30adb68b96ae70bda5b3dc88e50a7
Wesley Fonseca <wfonseca18@gmail.com>
2018-10-30 19:41:59 -0400

    Creating initial files

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 
-var stocksList = [
-    'GE',
-    'UA',
-    'US30'
+var stockList = [
+    {
+        ticker : 'GE',
+        name : 'GE'
+    },
+    {
+        ticker : 'UA',
+        name : 'Under Armour'
+    },
+    {
+        ticker : 'MSFT',
+        name : 'Microsoft'
+    },
+    {
+        ticker : 'FB',
+        name : 'Facebook'
+    },
+    {
+        ticker : 'AAPL',
+        name : 'Apple'
+    },
+    {
+        ticker : 'OKTA',
+        name : 'Okta'
+    },
+    {
+        ticker : 'NOW',
+        name : 'ServiceNow'
+    }
 ];
 
+stockList.forEach(stock => {
+    var ticker = stock.ticker.toLowerCase();
+    $('#stock-list').append(`<li class="my-2"><button id="${ticker}">${stock.name}</button></li>`);
+    $(`#${ticker}`).on('click', function(){
+        $.get(`https://api.iextrading.com/1.0/stock/${ticker}/logo`, function(response){
+            console.log();
+            $('#logo').attr('src', response.url);
+            $('#logo').show();
+        });
+    });
+});
 
-
-
-
-
-
-
-
-
+// https://api.iextrading.com/1.0/stock/aapl/batch?types=quote,news,chart&range=1m&last=10
 
 
