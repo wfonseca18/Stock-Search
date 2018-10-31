c211f87ce831f509e7fec3374de7028b25bce052
Wesley Fonseca <wfonseca18@gmail.com>
2018-10-31 18:46:39 -0400

    Adding spacing to inde.html

--- index.js
+++ index.js
@@ -32,37 +32,54 @@ var stockList = [
 
 var validationList = [];
 
-stockList.forEach(stock => {
-    var ticker = stock.ticker.toLowerCase();
-    $('#stock-list').append(`<li class="my-2"><button id="${ticker}">${stock.name}</button></li>`);
-    $(`#${ticker}`).on('click', function () {
-        $.get(`https://api.iextrading.com/1.0/stock/${ticker}/batch?types=logo,quote,news,chart&range=1m&last=10`, function (response) {
-            let html = '', articles = [];
-            $('#logo').attr('src', response.logo.url);
-            $('#logo').show();
-            $('.co-name').text(response.quote.companyName);
-            $('.price').text(`$${response.quote.latestPrice}`);
-            articles = response.news;
-            for (let i = 0; i < articles.length; i++) {
-                html += '<div class="card my-3">';
-                html += `<div class="card-header"><b>${i + 1}. ${articles[i].headline}</b></div>`;
-                html += `<div class="card-body"><p>Source: ${articles[i].source}<br>Date: ${articles[i].datetime}</p>`;
-                html += `<p>Summary: ${articles[i].summary}</p>`;
-                html += '</div>';
-                html += `<div class="card-footer"><a href="${articles[i].url}" target="_blank">Read more</a></div>`;
-                html += '</div>';
-            };
-            $('.news').html(html);
+function populatePage() {
+    $('#stock-list').empty();
+    stockList.forEach(stock => {
+        var ticker = stock.ticker.toLowerCase();
+        $('#stock-list').append(`<li class="my-2"><button id="${ticker}">${stock.name}</button></li>`);
+        $(`#${ticker}`).on('click', function () {
+            $.get(`https://api.iextrading.com/1.0/stock/${ticker}/batch?types=logo,quote,news,chart&range=1m&last=10`, function (response) {
+                let html = '', articles = [];
+                $('#logo').attr('src', response.logo.url);
+                $('#logo').show();
+                $('.co-name').text(response.quote.companyName);
+                $('.price').text(`$${response.quote.latestPrice}`);
+                articles = response.news;
+                for (let i = 0; i < articles.length; i++) {
+                    html += '<div class="card my-3">';
+                    html += `<div class="card-header"><b>${i + 1}. ${articles[i].headline}</b></div>`;
+                    html += `<div class="card-body"><p>Source: ${articles[i].source}<br>Date: ${articles[i].datetime}</p>`;
+                    html += `<p>Summary: ${articles[i].summary}</p>`;
+                    html += '</div>';
+                    html += `<div class="card-footer"><a href="${articles[i].url}" target="_blank">Read more</a></div>`;
+                    html += '</div>';
+                };
+                $('.news').html(html);
+            });
         });
     });
-});
+}
+populatePage();
 
-$.get('https://api.iextrading.com/1.0/ref-data/symbols', function(response){
+$.get('https://api.iextrading.com/1.0/ref-data/symbols', function (response) {
     response.forEach(element => {
         validationList.push({
-            'ticker' : element.symbol.toUpperCase(),
-            'name' : element.name
+            'ticker': element.symbol.toUpperCase(),
+            'name': element.name
         });
     });
 });
 
+$('#add').on('click', function(){
+    let userInput = $('#addTicker').val().toUpperCase();
+    validationList.forEach(stock => {
+        if(userInput === stock.ticker){
+            stockList.push({
+                ticker : stock.ticker,
+                name : stock.name
+            });
+            populatePage();
+        }
+    });
+    $('#addTicker').val('');
+});
